784924c846a4b0cbe7ded800e16df8863f3c2081
赖小凡 <laixf16@chinaunicom.cn>
2022-02-28 16:41:29 +0800

    96. 不同的二叉搜索树

--- 96. 不同的二叉搜索树/src/Solution.java
+++ 96. 不同的二叉搜索树/src/Solution.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 public class Solution {
 
+    // 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
     public int numTrees(int n) {
         int[] dp = new int[n+1];
         dp[0] = 1;
