5bcefb25768eec941f76e9b7a82fdfb497a9db78
赖小凡 <laixf16@chinaunicom.cn>
2022-03-05 16:04:33 +0800

    114. 二叉树展开为链表

---
+++ 114. 二叉树展开为链表/src/Solution.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yw
+ * @since 2022/02/20
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode node = new TreeNode(1,new TreeNode(2,new TreeNode(3),new TreeNode(4)),new TreeNode(5,null,new TreeNode(6)));
+        s.flatten(node);
+    }
+
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode tmp = root.right;
+        root.right = root.left;
+        root.left = null;
+        while(root.right != null) root = root.right;
+        root.right = tmp;
+    }
+
+}
