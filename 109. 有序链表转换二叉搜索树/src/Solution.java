9f803f5882c7eed47f33f17ff15c0a65b96223ca
赖小凡 <laixf16@chinaunicom.cn>
2022-03-05 12:31:05 +0800

    109. 有序链表转换二叉搜索树

---
+++ 109. 有序链表转换二叉搜索树/src/Solution.java
@@ -0,0 +1,62 @@
+/**
+ * @author yw
+ * @since 2022/02/20
+ */
+
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode a = new ListNode(-10,new ListNode(-3,new ListNode(0,new ListNode(5,new ListNode(9)))));
+        Solution solution = new Solution();
+        TreeNode treeNode = solution.sortedListToBST(a);
+        System.out.println(treeNode);
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        else if(head.next == null) return new TreeNode(head.val);
+        ListNode pre = head;
+        ListNode p = pre.next;
+        ListNode q = p.next;
+        //找到链表的中点p
+        while(q!=null && q.next!=null){
+            pre = pre.next;
+            p = pre.next;
+            q = q.next.next;
+        }
+        //将中点左边的链表分开
+        pre.next = null;
+        TreeNode root = new TreeNode(p.val);
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(p.next);
+        return root;
+    }
+
+}
