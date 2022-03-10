6d9092ffd4f03a3ff1d53fb2642f110971842523
赖小凡 <laixf16@chinaunicom.cn>
2022-03-10 10:45:00 +0800

    278. 第一个错误的版本

---
+++ 278. 第一个错误的版本/src/Solution.java
@@ -0,0 +1,23 @@
+
+
+
+/**
+ * @author yw
+ * @since 2022/02/20
+ */
+
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        int left = 1;
+//        int right = n;
+//        while(left < right) {
+//            int mid = (right-left) / 2 + left;
+//            if(isBadVersion(mid)) {
+//                right = mid;
+//            } else {
+//                left = mid + 1;
+//            }
+//        }
+//        return right;
+//    }
+//}
